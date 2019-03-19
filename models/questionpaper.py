fd7ede803584f2ca083883f19ccdc58cf0eb62a9
Gowtham Behara <gothsiddu@gmail.com>
2019-03-19 12:59:46 +0530

    Define getter methods for private fields

--- models/questionpaper.py
+++ models/questionpaper.py
@@ -3,6 +3,15 @@ from models.difficulty import DifficultyLevel
 
 class QuestionPaper:
     def __init__(self, question_number, difficulty, marks):
-        self.question_number = question_number
-        self.difficulty = DifficultyLevel[difficulty]
-        self.marks = marks
+        self._question_number = question_number
+        self._difficulty = DifficultyLevel[difficulty]
+        self._marks = marks
+
+    def get_question_number(self):
+        return self._question_number
+
+    def get_difficulty(self):
+        return self._difficulty
+
+    def get_marks(self):
+        return self._marks
