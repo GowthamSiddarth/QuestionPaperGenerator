7488b53cf1a20e0a9c49685115517d909f2b654e
Gowtham Behara <gothsiddu@gmail.com>
2019-03-19 15:50:15 +0530

    Get marks distribution; Use QuestionsFilter to filter questions with sum of marks equal to target marks

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from helper.dataparser import DataParser
 from helper.dataprocessor import DataProcessor
+from util.questionsfilter import QuestionsFilter
+import itertools
 
 if __name__ == '__main__':
     data_set_path = 'data/sample-input.txt'
@@ -8,3 +10,11 @@ if __name__ == '__main__':
 
     data_processor = DataProcessor()
     sorted_questions_group = data_processor.group_by_difficulty_with_questions_sorted(questions_group)
+
+    marks_distribution = data_parser.get_marks_distribution()
+    question_paper = list(itertools.chain(*[
+        QuestionsFilter.filter_questions_with_target_marks(sorted_questions_group[difficulty_group], target_marks) for
+        difficulty_group, target_marks in marks_distribution.items()]))
+
+    for question in question_paper:
+        print(question)
