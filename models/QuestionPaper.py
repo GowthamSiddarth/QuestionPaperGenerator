c6449d7177ae7d302002334e0ac6030c147664d6
Gowtham Behara <gothsiddu@gmail.com>
2019-03-19 10:41:28 +0530

    Create class for QuestionPaper model

---
+++ models/QuestionPaper.py
@@ -0,0 +1,8 @@
+from models.Difficulty import DifficultyLevel
+
+
+class QuestionPaper:
+    def __init__(self, question_number, difficulty, marks):
+        self.question_number = question_number
+        self.difficulty = DifficultyLevel[difficulty]
+        self.marks = marks
