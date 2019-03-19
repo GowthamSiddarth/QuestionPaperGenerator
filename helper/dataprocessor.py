905e09dc9e59ed1e500f956676fe8b9d890186e0
Gowtham Behara <gothsiddu@gmail.com>
2019-03-19 14:57:17 +0530

    Declare grouping method as static

--- helper/dataprocessor.py
+++ helper/dataprocessor.py
@@ -1,5 +1,6 @@
 class DataProcessor:
-    def group_by_difficulty_with_questions_sorted(self, questions_list):
+    @staticmethod
+    def group_by_difficulty_with_questions_sorted(questions_list):
         questions_group = {}
         for question in questions_list:
             questions_group[question.get_difficulty()] = questions_group.get(question.get_difficulty(), []) + [question]
