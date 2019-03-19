eafb5373c2825e962fc252b28f02210c454828c3
Gowtham Behara <gothsiddu@gmail.com>
2019-03-19 12:47:14 +0530

    Create static method for processing input and assigning to fields

--- helper/dataprocessor.py
+++ helper/dataprocessor.py
@@ -4,18 +4,29 @@ from models.difficulty import DifficultyLevel
 
 class DataProcessor:
     def __init__(self, data_set_path):
-        self.data_set_path = data_set_path
+        self._data_set_path = data_set_path
+        self._num_of_questions, self._questions, self._total_marks, self.marks_distribution = DataProcessor.read_input()
 
+    @staticmethod
+    def _map_meta_data_to_question_paper(meta_data):
+        cols = meta_data.split(', ')
+        return QuestionPaper(cols[0][1], cols[1], cols[2])
+
+    @staticmethod
     def read_input(self):
-        with open(self.data_set_path) as data_set_file:
+        with open(self._data_set_path) as data_set_file:
             content = data_set_file.readlines()
 
         content = [line.strip() for line in content]
-        self.num_of_questions = int(content[0])
-        self.questions = [QuestionPaper(cols[0][1], cols[1], cols[2]) for line in content[1:-1] for cols in
-                          line.split(', ')]
+        num_of_questions = int(content[0])
+        for line in content[1:-1]:
+            print(line.split(', '))
+        questions = [self._map_meta_data_to_question_paper(line) for line in content[1:-1]]
         last_line_cols = content[-1].split(', ')
-        self.total_marks = int(last_line_cols[0])
+        total_marks = int(last_line_cols[0])
+        marks_distribution = {}
         for idx in range(1, len(last_line_cols)):
             cols = last_line_cols[idx].split(' ')
-            self.marks_distribution[DifficultyLevel[cols[0]]] = int(cols[1])
+            marks_distribution[DifficultyLevel[cols[0]]] = int(cols[1])
+
+        return num_of_questions, questions, total_marks, marks_distribution
