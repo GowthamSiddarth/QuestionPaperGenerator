8d6e79691a81224d7cb8395845ece3cde5610bc0
Gowtham Behara <gothsiddu@gmail.com>
2019-03-19 12:58:15 +0530

    Rename methods; pass self to static method call; Remove loggers

--- helper/dataprocessor.py
+++ helper/dataprocessor.py
@@ -5,9 +5,10 @@ from models.difficulty import DifficultyLevel
 class DataProcessor:
     def __init__(self, data_set_path):
         self._data_set_path = data_set_path
-        self._num_of_questions, self._questions, self._total_marks, self._marks_distribution = DataProcessor.read_input()
+        self._num_of_questions, self._questions, self._total_marks, self._marks_distribution = DataProcessor._read_input(
+            self)
 
-    def get_num_of_question(self):
+    def get_num_of_questions(self):
         return self._num_of_questions
 
     def get_questions(self):
@@ -25,14 +26,12 @@ class DataProcessor:
         return QuestionPaper(cols[0][1], cols[1], cols[2])
 
     @staticmethod
-    def read_input(self):
+    def _read_input(self):
         with open(self._data_set_path) as data_set_file:
             content = data_set_file.readlines()
 
         content = [line.strip() for line in content]
         num_of_questions = int(content[0])
-        for line in content[1:-1]:
-            print(line.split(', '))
         questions = [self._map_meta_data_to_question_paper(line) for line in content[1:-1]]
         last_line_cols = content[-1].split(', ')
         total_marks = int(last_line_cols[0])
