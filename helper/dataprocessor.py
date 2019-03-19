e0b524780d5d47c880cdc9346940e3a7badf340b
Gowtham Behara <gothsiddu@gmail.com>
2019-03-19 14:56:35 +0530

    Create class for grouping questions by difficulty and sorting questions from a group by marks and question number

---
+++ helper/dataprocessor.py
@@ -0,0 +1,11 @@
+class DataProcessor:
+    def group_by_difficulty_with_questions_sorted(self, questions_list):
+        questions_group = {}
+        for question in questions_list:
+            questions_group[question.get_difficulty()] = questions_group.get(question.get_difficulty(), []) + [question]
+
+        for difficulty_group, questions_from_group in questions_group.items():
+            questions_group[difficulty_group] = sorted(questions_from_group, key=lambda question: (
+                question.get_marks(), question.get_question_number()))
+
+        return questions_group
